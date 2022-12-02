0b335f6325d135039a70b40bf42eb4f82be53abb
Ivan Somkin <somkin.van@yandex.ru>
2022-12-02 22:19:44 +0300

    Refactor plan-meeting

--- public/javascript/plan-meeting-organizer.js
+++ public/javascript/plan-meeting-organizer.js
@@ -1,10 +1,14 @@
+const dayNames = ['Monday', 'Tuesday', 'Wednesday', 'Thurday', 'Friday', 'Saturday', 'Sunday']
+
+// Pad number with leading zeros
 function pad(num, size) {
   num = num.toString()
   while (num.length < size) num = "0" + num
   return num
 }
 
-function minutesToClock(minutes) {
+// Transforms number of minutes to clock representation
+function ToClock(minutes) {
   let clockHours = minutes / 60
   let clockMinutes = minutes % 60
 
@@ -15,80 +19,96 @@ function minutesToClock(minutes) {
   return pad(clockHours, 2) + ":" + pad(clockMinutes, 2)
 }
 
+// Function for timeslot block ids
 function timeslotId(dayNum, timeslotNum) {
   return dayNum + "-" + timeslotNum
 }
 
-function timeslotText(startMinutes, lengthMinutes) {
-  const startClock = minutesToClock(startMinutes)
-  const endClock = minutesToClock(startMinutes + lengthMinutes)
+// Creates text to put inside timeslot
+function timeslotText(start, length) {
+  const startClock = ToClock(start)
+  const endClock = ToClock(start + length)
   return startClock + " - " + endClock
 }
 
-function makeScheduleTable() {
-  const weekCount = parseInt($('#week-count').val())
-  const timeslotLengthMinutes = parseInt($('#timeslot-length').val())
-  const startingTimeMinutes = parseInt($('#start-time').text())
-  const dayNames = ['Monday', 'Tuesday', 'Wednesday', 'Thurday', 'Friday', 'Saturday', 'Sunday']
+// Gets day of the week from day number
+function getDay(dayNum) {
+  return dayNames[dayNum % 7]
+}
 
+// Builds one timeslot block
+function buildScheduleTimeslot(dayNum, timeslotNum, currentTime, timeslotLength) {
+  const timeslot = $('<div></div>').addClass('day__timeslots__item').attr('id', timeslotId(dayNum, timeslotNum))
+  timeslot.addClass('small-text-bold').text(timeslotText(currentTime, timeslotLength))
+  return timeslot
+}
+
+// Builds one day containing timeslots
+function buildScheduleDay(dayNum, timeslotLength, startingTime, timeslotCount) {
+  const day = $('<div></div>').addClass('day')
+  const dayName = $('<div></div>').addClass('day__name').addClass('small-text-bold').text(getDay(dayNum))
+  const dayTimeslots = $('<div></div>').addClass('day__timeslots')
+
+  let currentTime = startingTime
+  for (let timeslotNum = 0; timeslotNum < timeslotCount; timeslotNum++) {
+    dayTimeslots.append(buildScheduleTimeslot(dayNum, timeslotNum, currentTime, timeslotLength))
+    currentTime += timeslotLength
+  }
+
+  day.append(dayName)
+  day.append(dayTimeslots)
+  return day
+}
+
+// Builds the whole table with all days containing timeslots
+function buildScheduleTable() {
+  const weekCount = parseInt($('#week-count').val())
+  const timeslotLength = parseInt($('#timeslot-length').val())
+  const startingTime = parseInt($('#start-time').text())
   const schedule = $('#schedule')
+  const dayCount = weekCount * 7
+  const timeslotCount = (1440 - startingTime) / timeslotLength
   schedule.empty()
 
-  const dayCount = weekCount * 7
-  const timeslotCount = (1440 - startingTimeMinutes) / timeslotLengthMinutes
-
   for (let dayNum = 0; dayNum < dayCount; dayNum++) {
-    const day = $('<div></div>').addClass('schedule__day')
-    const dayName = $('<div></div>').addClass('schedule__day__name').addClass('small-text-bold').text(dayNames[dayNum % 7])
-    const dayTimeslots = $('<div></div>').addClass('schedule__day__timeslots')
-
-    let currentTimeMinutes = startingTimeMinutes
-
-    for (let timeslotNum = 0; timeslotNum < timeslotCount; timeslotNum++) {
-      const timeslot = $('<div></div>').addClass('schedule__day__timeslots__item').attr('id', timeslotId(dayNum, timeslotNum))
-      timeslot.text(timeslotText(currentTimeMinutes, timeslotLengthMinutes))
-      dayTimeslots.append(timeslot)
-      currentTimeMinutes += timeslotLengthMinutes
-    }
-
-    day.append(dayName)
-    day.append(dayTimeslots)
-    schedule.append(day)
+    schedule.append(buildScheduleDay(dayNum, timeslotLength, startingTime, timeslotCount))
   }
 }
 
-function loadSchedule() {
-}
-
+// Show plan conditions in existing schedule table
 async function loadConditions() {
   data = await $.ajax({
     url: "/api/plan/meeting/" + planUuid + "/answer/conditions"
   })
   data.blockedTimeslots.forEach((timeslot) => {
     blockedTimeslot = $('#' + timeslotId(timeslot.dayNum, timeslot.timeslotNum))
-    blockedTimeslot.addClass("blocked-timeslot")
+    blockedTimeslot.addClass("blocked")
   })
-  $('.schedule__day__timeslots__item').on('click', (e) => {
-    e.target.classList.toggle('blocked-timeslot')
+  $('.day__timeslots__item').on('click', (e) => {
+    e.target.classList.toggle('blocked')
   })
 }
 
+// Show plan answer in existing schedule table
 async function loadAnswer(answerId) {
   data = await $.ajax({
     url: "answer/" + answerId
   })
 }
 
+// Show plan result in existing schedule table
 async function loadResult() {
   data = await $.ajax({
     url: "calculate"
   })
 }
 
+
 async function updateConditions() {
 
 }
 
 window.onload = function () {
-  makeScheduleTable()
+  buildScheduleTable()
+  loadConditions()
 }
