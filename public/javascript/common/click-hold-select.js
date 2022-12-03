aba7f94b75bbe4d170c38ca54f3da1abbc0f1afb
Ikar108 <61326455+Ikar108@users.noreply.github.com>
2022-12-03 22:30:14 +0300

    Add answer page alfa version

--- public/javascript/common/click-hold-select.js
+++ public/javascript/common/click-hold-select.js
@@ -4,56 +4,65 @@ var CHANGE_CLASS_MAP = new Map()
 var CURRENT_CLICK_RATING = 'rating-none'
 
 function addTimeslotRatingSelect(type) {
-  timeslots = $('.schedule .day__timeslots__item')
-  addHoldClickRating(timeslots, type)
+    timeslots = $('.schedule .day__timeslots__item')
+    addHoldClickRating(timeslots, type)
 }
 
 function addHoldClickRating(timeslots, type) {
-  timeslots.on('mousedown', function () {
-    HOLD = true
-    const previous_rating = $(this).attr('class').match(/rating-([^\s])+/g)[0]
-    if (previous_rating != CURRENT_CLICK_RATING) {
-      $(this).addClass(CURRENT_CLICK_RATING)
-      $(this).removeClass(previous_rating)
-      CHANGE_CLASS_MAP.set($(this).attr('id'), CURRENT_CLICK_RATING)
-    }
-  })
-
-  $('body').on('mouseup', function () {
-    HOLD = false
-  })
-
-  timeslots.on('mouseup', function () {
-    CHANGE_CLASS_MAP.set($(this).attr('id'), CURRENT_CLICK_RATING)
-  })
-
-  timeslots.on('mouseenter', function () {
-    const previous_rating = $(this).attr('class').match(/rating-([^\s])+/)[0]
-    if (!HOLD) {
-      if (type == 'block') {
-        if ($(this).hasClass('rating-blocked')) {
-          CURRENT_CLICK_RATING = 'rating-none'
-        } else {
-          CURRENT_CLICK_RATING = 'rating-blocked'
+    timeslots.on('mousedown', function() {
+        if ($(this).hasClass('rating-blocked') && type != 'block') {
+            return
         }
-      }
-      CHANGE_CLASS_MAP.set($(this).attr('id'), previous_rating)
-    } else {
-      CHANGE_CLASS_MAP.set($(this).attr('id'), CURRENT_CLICK_RATING)
-    }
-    if (previous_rating != CURRENT_CLICK_RATING) {
-      $(this).addClass(CURRENT_CLICK_RATING)
-      $(this).removeClass(previous_rating)
-    }
-  })
+        HOLD = true
+        const previous_rating = $(this).attr('class').match(/rating-([^\s])+/)[0]
+        if (previous_rating != CURRENT_CLICK_RATING) {
+            $(this).addClass(CURRENT_CLICK_RATING)
+            $(this).removeClass(previous_rating)
+            CHANGE_CLASS_MAP.set($(this).attr('id'), CURRENT_CLICK_RATING)
+        }
+    })
 
-  timeslots.on('mouseleave', function () {
-    if (!HOLD) {
-      $(this).addClass(CHANGE_CLASS_MAP.get($(this).attr('id')))
-      const ratings = ($(this).attr('class').match(/rating-(.)+/g) || [])[0].split(' ')
-      if (ratings.length > 1) {
-        $(this).removeClass(ratings[0])
-      }
-    }
-  })
+    $('body').on('mouseup', function() {
+        HOLD = false
+    })
+
+    timeslots.on('mouseup', function() {
+        CHANGE_CLASS_MAP.set($(this).attr('id'), CURRENT_CLICK_RATING)
+    })
+
+    timeslots.on('mouseenter', function() {
+        if ($(this).hasClass('rating-blocked') && type != 'block') {
+            return
+        }
+        const previous_rating = $(this).attr('class').match(/rating-([^\s])+/)[0]
+        if (!HOLD) {
+            if (type == 'block') {
+                if ($(this).hasClass('rating-blocked')) {
+                    CURRENT_CLICK_RATING = 'rating-none'
+                } else {
+                    CURRENT_CLICK_RATING = 'rating-blocked'
+                }
+            }
+            CHANGE_CLASS_MAP.set($(this).attr('id'), previous_rating)
+        } else {
+            CHANGE_CLASS_MAP.set($(this).attr('id'), CURRENT_CLICK_RATING)
+        }
+        if (previous_rating != CURRENT_CLICK_RATING) {
+            $(this).addClass(CURRENT_CLICK_RATING)
+            $(this).removeClass(previous_rating)
+        }
+    })
+
+    timeslots.on('mouseleave', function() {
+        if ($(this).hasClass('rating-blocked') && type != 'block') {
+            return
+        }
+        if (!HOLD) {
+            $(this).addClass(CHANGE_CLASS_MAP.get($(this).attr('id')))
+            const ratings = ($(this).attr('class').match(/rating-(.)+/) || [])[0].split(' ')
+            if (ratings.length > 1) {
+                $(this).removeClass(ratings[0])
+            }
+        }
+    })
 }
