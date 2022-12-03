c72a4517750684a3a599a74f5c05ae95a9751ebb
Ivan Somkin <somkin.van@yandex.ru>
2022-12-03 19:20:13 +0300

    Update click-hold-select script to work

--- public/javascript/common/click-hold-select.js
+++ public/javascript/common/click-hold-select.js
@@ -1,44 +1,53 @@
 var HOLD = false
 var CURRENT_CHANGE_CLASS = ''
 var CHANGE_CLASS_MAP = new Map()
+var CURRENT_CLICK_RATING = 'rating-none'
 
-function addBlockedEventListeners() {
+function addTimeslotRatingSelect(type) {
+  timeslots = $('.schedule .day__timeslots__item')
+  addHoldClickRating(timeslots, type)
+}
 
-  $('.schedule .day__timeslots__item').on('mousedown', function () {
+function addHoldClickRating(timeslots, type) {
+  timeslots.on('mousedown', function () {
     HOLD = true
-    if ($(this).hasClass('rating-blocked')) {
-      CURRENT_CLICK_CLASS = 'rating-blocked'
-    } else {
-      CURRENT_CLICK_CLASS = 'rating-none'
-    }
-    const ratings = ($(this).attr('class').match(/rating-(.)+/g) || [])
-    $(this).addClass(CURRENT_CLICK_CLASS)
-    if (ratings.length > 1) {
-      $(this).removeClass(ratings[0].split(' ')[0])
+    const previous_rating = $(this).attr('class').match(/rating-([^\s])+/g)[0]
+    if (previous_rating != CURRENT_CLICK_RATING) {
+      $(this).addClass(CURRENT_CLICK_RATING)
+      $(this).removeClass(previous_rating)
+      CHANGE_CLASS_MAP.set($(this).attr('id'), CURRENT_CLICK_RATING)
     }
   })
 
   $('body').on('mouseup', function () {
-    CHANGE_CLASS_MAP.set($(this).attr('id'), CURRENT_CLICK_CLASS)
     HOLD = false
   })
 
-  $('.schedule .day__timeslots__item').on('mouseenter', function () {
-    if (!HOLD) {
-      if ($(this).hasClass('rating-blocked')) {
-        CURRENT_CLICK_CLASS = 'rating-none'
-      } else {
-        CURRENT_CLICK_CLASS = 'rating-blocked'
-      }
-    }
-    const current_rating = ($(this).attr('class').match(/rating-(.)+/g) || [])[0].split(' ')[0]
-    console.log('set: ' + current_rating)
-    CHANGE_CLASS_MAP.set($(this).attr('id'), current_rating)
-    $(this).addClass(CURRENT_CLICK_CLASS)
-    $(this).removeClass(current_rating)
+  timeslots.on('mouseup', function () {
+    CHANGE_CLASS_MAP.set($(this).attr('id'), CURRENT_CLICK_RATING)
   })
 
-  $('.schedule .day__timeslots__item').on('mouseleave', function () {
+  timeslots.on('mouseenter', function () {
+    const previous_rating = $(this).attr('class').match(/rating-([^\s])+/)[0]
+    if (!HOLD) {
+      if (type == 'block') {
+        if ($(this).hasClass('rating-blocked')) {
+          CURRENT_CLICK_RATING = 'rating-none'
+        } else {
+          CURRENT_CLICK_RATING = 'rating-blocked'
+        }
+      }
+      CHANGE_CLASS_MAP.set($(this).attr('id'), previous_rating)
+    } else {
+      CHANGE_CLASS_MAP.set($(this).attr('id'), CURRENT_CLICK_RATING)
+    }
+    if (previous_rating != CURRENT_CLICK_RATING) {
+      $(this).addClass(CURRENT_CLICK_RATING)
+      $(this).removeClass(previous_rating)
+    }
+  })
+
+  timeslots.on('mouseleave', function () {
     if (!HOLD) {
       $(this).addClass(CHANGE_CLASS_MAP.get($(this).attr('id')))
       const ratings = ($(this).attr('class').match(/rating-(.)+/g) || [])[0].split(' ')
